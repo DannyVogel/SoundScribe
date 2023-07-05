4a16e97b556b737d25c3f06c25eb23c3cbabb35f
DannyVogel <danny.hj.vogel@gmail.com>
2023-07-05 21:02:09 +0200

    feat: hide SoundBoard when not logged in

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,17 +5,22 @@ const authStore = useAuthStore()
 const pages = [
   {
     name: 'Feed',
-    to: '/feed'
+    to: '/feed',
+    loggedIn: false
   },
-  { name: 'SoundBoard', to: `/soundboard/:${authStore.userName}` },
-  { name: 'Discover', to: '/discover' }
+  { name: 'SoundBoard', to: `/soundboard/:${authStore.userName}`, loggedIn: true },
+  { name: 'Discover', to: '/discover', loggedIn: false }
 ]
 </script>
 <template>
-  <div class="flex justify-center gap-2 bg-blue-900 text-sm text-white">
-    <nav v-for="(page, index) in pages">
-      <RouterLink :to="page.to">{{ page.name }}</RouterLink>
-      <span v-if="index !== pages.length - 1"> | </span>
+  <div class="flex justify-center gap-1 bg-blue-900 text-sm text-white">
+    <nav v-for="(page, index) in pages" :key="index">
+      <template v-if="!page.loggedIn || (page.loggedIn && authStore.isLoggedIn)">
+        <RouterLink :to="page.to">{{ page.name }}</RouterLink>
+        <span :class="!authStore.isLoggedIn && 'pl-1'" v-if="index !== pages.length - 1">{{
+          ' | '
+        }}</span>
+      </template>
     </nav>
   </div>
 </template>
