f01a5793e3e54c8142b158c3c05690ad10fc3251
DannyVogel <danny.hj.vogel@gmail.com>
2023-07-11 23:35:51 +0200

    wip: temp style for lg screen

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="relative w-screen h-screen max-h-screen grid grid-rows-layout">
+  <div class="relative w-screen h-screen max-h-screen grid grid-rows-layout bg-gray-800">
     <slot name="header" />
-    <div class="max-h-full bg-gray-800 overflow-hidden">
+    <div class="max-h-full bg-gray-800 overflow-hidden lg:w-1/2 lg:mx-auto">
       <slot name="main" />
     </div>
   </div>
