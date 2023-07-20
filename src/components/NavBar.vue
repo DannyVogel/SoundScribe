b6bc9b5be31d95baf5d406a06548bf2c78e86155
DannyVogel <danny.hj.vogel@gmail.com>
2023-07-20 21:44:20 +0200

    refactor

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,25 +2,17 @@
 import { useAuthStore } from '@/stores/authStore'
 
 const authStore = useAuthStore()
-const pages = [
-  {
-    name: 'Feed',
-    to: '/feed',
-    loggedIn: false
-  },
-  { name: 'SoundBoard', to: `/soundboard/:${authStore.userName}`, loggedIn: true },
-  { name: 'Discover', to: '/discover', loggedIn: false }
-]
 </script>
 <template>
-  <div class="flex justify-center gap-1 text-sm text-white">
-    <nav v-for="(page, index) in pages" :key="index">
-      <template v-if="!page.loggedIn || (page.loggedIn && authStore.isLoggedIn)">
-        <RouterLink :to="page.to">{{ page.name }}</RouterLink>
-        <span :class="!authStore.isLoggedIn && 'pl-1'" v-if="index !== pages.length - 1">{{
-          ' | '
-        }}</span>
-      </template>
-    </nav>
-  </div>
+  <nav class="flex justify-center gap-1 text-sm text-white">
+    <RouterLink to="/feed">Feed</RouterLink>
+    <span>{{ ' | ' }}</span>
+    <div v-if="authStore.isLoggedIn">
+      <RouterLink :to="{ name: 'soundBoard', params: { user: authStore.userName } }"
+        >SoundBoard
+      </RouterLink>
+      <span>{{ ' | ' }}</span>
+    </div>
+    <RouterLink to="/discover">Discover</RouterLink>
+  </nav>
 </template>
