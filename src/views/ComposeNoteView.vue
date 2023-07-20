f5429ad63d938e14d570d497fee4a80885ba6900
DannyVogel <danny.hj.vogel@gmail.com>
2023-07-20 22:02:46 +0200

    fix: rearrange firebase data structure for notes, import util for URL, update names

--- src/views/ComposeNoteView.vue
+++ src/views/ComposeNoteView.vue
@@ -1,14 +1,15 @@
 <script setup>
 import { ref } from 'vue'
 import { useAuthStore } from '@/stores/authStore'
-import { db, doc, setDoc } from '@/services/Firebase'
+import { collection, db, doc, addDoc, serverTimestamp } from '@/services/Firebase'
 import { v4 as uuidv4 } from 'uuid'
 import { useRouter } from 'vue-router'
+import { getYouTubeEmbedUrl } from '@/utils'
 
 const authStore = useAuthStore()
 const router = useRouter()
-const isPosting = ref(false)
-const post = ref({
+const isComposing = ref(false)
+const note = ref({
   title: '',
   content: '',
   songURL: ''
@@ -18,39 +19,37 @@ if (!authStore.isLoggedIn) {
   router.push('/')
 }
 
-const postNote = () => {
-  isPosting.value = true
-  setDoc(doc(db, 'posts', authStore.userName), {
-    id: uuidv4(),
-    title: post.value.title,
-    content: post.value.content,
-    songURL: post.value.songURL
-  })
-  isPosting.value = false
+const postNote = async () => {
+  isComposing.value = true
+  const id = uuidv4()
+  const docData = {
+    id: id,
+    author: authStore.userName,
+    timeStamp: serverTimestamp(),
+    title: note.value.title,
+    content: note.value.content,
+    songURL: getYouTubeEmbedUrl(note.value.songURL)
+  }
+  await addDoc(collection(db, 'users', authStore.userUID, `userNotes`), docData)
+  isComposing.value = false
 }
 </script>
 <template>
   <div class="p-3 flex flex-col justify-center sm:max-w-2xl sm:mx-auto text-white">
     <h1>Compose Note</h1>
     <form @submit.prevent="postNote" class="mt-4 sm:max-w-2xl flex flex-col justify-center">
-      <fieldset class="w-full px-3 flex flex-col justify-center gap-2.5 border">
+      <fieldset class="w-full px-3 flex flex-col justify-center gap-2.5 border text-black">
         <legend>Your Note</legend>
         <label htmlFor="noteTitle">Title:</label>
-        <input v-model="post.title" type="text" name="noteTitle" id="noteTitle" />
+        <input v-model="note.title" type="text" name="noteTitle" id="noteTitle" />
         <label htmlFor="noteContent">Content:</label>
-        <textarea
-          v-model="post.content"
-          name="noteContent"
-          id="noteContent"
-          cols="{30}"
-          rows="{10}"
-        ></textarea>
+        <textarea v-model="note.content" name="noteContent" id="noteContent" rows="10"></textarea>
         <label htmlFor="songURL">Song Link:</label>
-        <input v-model="post.songURL" type="text" name="songURL" id="songURL" />
+        <input v-model="note.songURL" type="text" name="songURL" id="songURL" />
         <button
           class="w-full h-12 rounded bg-orange-500 hover:bg-orange-600 cursor-pointer text-white text-xl flex justify-center items-center mb-4"
-          :class="isPosting && 'animate-pulse'"
-          :disabled="isPosting"
+          :class="isComposing && 'animate-pulse'"
+          :disabled="isComposing"
         >
           Compose Note
         </button>
