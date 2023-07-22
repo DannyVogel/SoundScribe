513c1d732104b9ecb0aadc3419481295b0fa4796
DannyVogel <danny.hj.vogel@gmail.com>
2023-07-22 12:49:13 +0200

    wip: render notes

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -1,4 +1,28 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+import useAuthStore from '@/stores/authStore'
+import useNotesStore from '@/stores/notesStore'
+
+const authStore = useAuthStore()
+const notesStore = useNotesStore()
+
+// Fetch user notes when the component is mounted
+onMounted(() => {
+  notesStore.getAllUserNotes(authStore.userUID)
+})
+
+// Access the userNotes Map from the store
+const userNotes = notesStore.userNotes
+</script>
 <template>
-  <div></div>
+  <div v-if="userNotes.size > 0">
+    <div v-for="(note, noteId) in userNotes" :key="noteId">
+      <h3>Title: {{ note.title }}</h3>
+      <p>Content: {{ note.content }}</p>
+      <p>Author: {{ note.author }}</p>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading user notes...</p>
+  </div>
 </template>
