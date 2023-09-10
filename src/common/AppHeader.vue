4f21e3b169d193b81c80964af52ebd2ceefd330c
DannyVogel <danny.hj.vogel@gmail.com>
2023-09-10 09:56:12 +0200

    styles: header element positioning

--- src/common/AppHeader.vue
+++ src/common/AppHeader.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import SideNav from './SideNav.vue'
 import NavBar from '@/components/NavBar.vue'
 import { useRouter } from 'vue-router'
+import { Bars3Icon } from '@heroicons/vue/24/solid'
 
 const router = useRouter()
 const isSideNavShown = ref(false)
@@ -10,28 +11,32 @@ const isSideNavShown = ref(false)
 
 <template>
   <header
-    class="h-full p-2 flex justify-between items-center bg-gray-900 border-b-2 border-orange-600"
+    class="h-full px-2 flex justify-between items-center bg-gray-900 border-b-2 border-orange-600"
   >
-    <h1
-      @click="router.push('/')"
-      class="font-title text-white text-2xl cursor-pointer hover:animate-pulse"
-      to="/"
-    >
-      SoundScribe
-    </h1>
-    <NavBar />
-    <button @click="isSideNavShown = true">
-      <i class="fa-solid fa-bars text-white"></i>
-    </button>
-    <div
-      @click="isSideNavShown = false"
-      class="absolute w-screen h-screen top-0 left-0 opacity-40 bg-slate-800"
-      :class="!isSideNavShown && 'hidden'"
-    ></div>
-    <SideNav
-      @close="isSideNavShown = false"
-      class="w-64 h-full p-3 fixed top-0 right-0 bg-gray-900 shadow-md shadow-gray-700 translate-x-0 transition-transform ease-in-out duration-300 flex flex-col justify-start"
-      :class="!isSideNavShown && 'translate-x-full'"
-    />
+    <div class="w-full flex flex-col justify-start">
+      <h1
+        @click="router.push('/')"
+        class="font-title text-white text-2xl cursor-pointer hover:animate-pulse"
+        to="/"
+      >
+        SoundScribe
+      </h1>
+      <NavBar />
+    </div>
+    <div class="flex items-start">
+      <button @click="isSideNavShown = true" class="w-8 h-8">
+        <Bars3Icon class="text-white" />
+      </button>
+      <div
+        @click="isSideNavShown = false"
+        class="absolute w-screen h-screen top-0 left-0 opacity-40 bg-slate-800"
+        :class="!isSideNavShown && 'hidden'"
+      ></div>
+      <SideNav
+        @close="isSideNavShown = false"
+        class="w-64 h-full p-3 fixed top-0 right-0 bg-gray-900 shadow-md shadow-gray-700 translate-x-0 transition-transform ease-in-out duration-300 flex flex-col justify-start"
+        :class="!isSideNavShown && 'translate-x-full'"
+      />
+    </div>
   </header>
 </template>
