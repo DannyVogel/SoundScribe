ddc33a5a6e878ed10f7027444c581d06508b56e8
DannyVogel <danny.hj.vogel@gmail.com>
2023-09-10 09:56:24 +0200

    fix: layout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,15 +4,15 @@ import useAuthStore from '@/stores/authStore'
 const authStore = useAuthStore()
 </script>
 <template>
-  <nav class="flex justify-center gap-1 text-sm text-white">
+  <nav class="flex justify-center gap-4 text-sm text-white">
     <RouterLink to="/feed">Feed</RouterLink>
     <span>{{ ' | ' }}</span>
     <div v-if="authStore.userName">
       <RouterLink :to="{ name: 'soundBoard', params: { user: authStore.userName } }"
         >SoundBoard
       </RouterLink>
-      <span>{{ ' | ' }}</span>
     </div>
+    <span v-if="authStore.userName">{{ ' | ' }}</span>
     <RouterLink to="/discover">Discover</RouterLink>
   </nav>
 </template>
