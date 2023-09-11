e8e569d50e01f3fc9433409e4efff79f2a6b2bfd
DannyVogel <danny.hj.vogel@gmail.com>
2023-09-11 20:03:13 +0200

    install QuillEditor

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
-
 import App from './App.vue'
 import router from './router'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
 
 const app = createApp(App)
 
@@ -14,6 +15,7 @@ const options = {
   // You can set your default options here
 }
 
+app.component('QuillEditor', QuillEditor)
 app.use(createPinia())
 app.use(router)
 app.use(Toast, options)
