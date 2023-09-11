9ca34a6437969ea5c3ca8137267a211ee2d0853c
DannyVogel <danny.hj.vogel@gmail.com>
2023-09-11 18:21:10 +0200

    remove condition for soundboard link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,12 +7,10 @@ const authStore = useAuthStore()
   <nav class="flex justify-center gap-4 text-sm text-white">
     <RouterLink to="/feed">Feed</RouterLink>
     <span>{{ ' | ' }}</span>
-    <div v-if="authStore.userName">
-      <RouterLink :to="{ name: 'soundBoard', params: { user: authStore.userName } }"
-        >SoundBoard
-      </RouterLink>
-    </div>
-    <span v-if="authStore.userName">{{ ' | ' }}</span>
+    <RouterLink :to="{ name: 'soundBoard', params: { user: authStore.userName } }"
+      >SoundBoard
+    </RouterLink>
+    <span>{{ ' | ' }}</span>
     <RouterLink to="/discover">Discover</RouterLink>
   </nav>
 </template>
