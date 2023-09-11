45eb56c286352ad9c1610b086f9b060ac1c1eb43
DannyVogel <danny.hj.vogel@gmail.com>
2023-09-12 00:05:34 +0200

    fix layout

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="relative w-screen h-screen max-h-screen grid grid-rows-layout bg-gray-800">
     <slot name="header" />
-    <div class="max-h-full bg-gray-800 overflow-hidden md:w-1/2 md:mx-auto">
+    <div class="max-h-full bg-gray-800 overflow-x-hidden md:w-1/2 md:mx-auto">
       <slot name="main" />
     </div>
   </div>
