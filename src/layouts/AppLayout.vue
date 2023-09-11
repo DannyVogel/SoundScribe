66f4b0a640507538f5f993f417995e171effe4ab
DannyVogel <danny.hj.vogel@gmail.com>
2023-09-11 18:21:19 +0200

    update responsive styles

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="relative w-screen h-screen max-h-screen grid grid-rows-layout bg-gray-800">
     <slot name="header" />
-    <div class="max-h-full bg-gray-800 overflow-hidden lg:w-1/2 lg:mx-auto">
+    <div class="max-h-full bg-gray-800 overflow-hidden md:w-1/2 md:mx-auto">
       <slot name="main" />
     </div>
   </div>
