7125a0250e80911518f7eb83f30cb4a2bee86638
DannyVogel <danny.hj.vogel@gmail.com>
2023-09-11 18:40:50 +0200

    style: scrollbar

--- src/App.vue
+++ src/App.vue
@@ -24,3 +24,27 @@ onMounted(() => {
     </template>
   </AppLayout>
 </template>
+<style>
+/* html {
+  overflow-x: hidden;
+} */
+
+::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+  background-color: transparent !important;
+}
+
+::-webkit-scrollbar-track {
+  border-radius: 1rem;
+  margin: 0.4rem;
+  margin-top: 0.4rem;
+  border-left: 1px solid rgba(211, 211, 211, 0);
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 20px;
+  background: linear-gradient(30deg, #ea580c, #ea580c);
+  box-shadow: inset 0 0 6px #ea580c;
+}
+</style>
