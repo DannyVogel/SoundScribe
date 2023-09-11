640076ea566f3ad8aecd160b7435f0028f88a9dc
DannyVogel <danny.hj.vogel@gmail.com>
2023-09-12 00:15:12 +0200

    feat: temp home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import useAuthStore from '@/stores/authStore'
+import SearchScribe from '@/common/SearchScribe.vue'
 const authStore = useAuthStore()
 </script>
 
 <template>
-  <main class="h-full px-3 flex flex-col justify-center items-center gap-2 bg-gray-800">
-    <h1 class="text-lg font-bold">Home</h1>
-    <p>Logged in: {{ authStore.isLoggedIn || false }}</p>
-    <p>User: {{ authStore.userName || 'N/A' }}</p>
-    <p>Account type: {{ authStore.accountType || 'N/A' }}</p>
+  <main class="h-full py-12 px-3 flex flex-col items-center gap-2 bg-gray-800 text-white">
+    <h1 class="text-4xl font-title animate-pulse">SoundScribe</h1>
+    <p>Where music meets storytelling.</p>
+    <p>Check out the latest Notes in the Feed or search for a specific Scribe below.</p>
+    <p>
+      You can log in or create an account in the side menu by clicking the icon on the top-right.
+    </p>
+    <SearchScribe />
   </main>
 </template>
