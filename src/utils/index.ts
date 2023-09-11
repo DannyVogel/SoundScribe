d7ed6ad4cfa6470e2a8f4918d98aae47dac7d3da
DannyVogel <danny.hj.vogel@gmail.com>
2023-09-11 23:07:09 +0200

    feat: youtubeurl regex

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,3 +12,6 @@ export const getYouTubeEmbedUrl = (url) => {
 }
 
 export const uuidv4Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+
+export const youtubeUrlRegex =
+  /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?(?=.*v=([-\w]+))(?:\S+)?|embed\/([-\w]+)|v\/([-\w]+)|user\/\w+)?|youtu\.be\/([-\w]+))/
