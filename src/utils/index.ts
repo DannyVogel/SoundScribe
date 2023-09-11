aa0c5beb4d022ebe8725fff8c002fd43ecf33fea
DannyVogel <danny.hj.vogel@gmail.com>
2023-09-12 00:05:46 +0200

    add regex for signup validations

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -15,3 +15,9 @@ export const uuidv4Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]
 
 export const youtubeUrlRegex =
   /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?(?=.*v=([-\w]+))(?:\S+)?|embed\/([-\w]+)|v\/([-\w]+)|user\/\w+)?|youtu\.be\/([-\w]+))/
+
+export const usernameRegex = /^[a-zA-Z0-9_-]{3,16}$/
+
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+
+export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
