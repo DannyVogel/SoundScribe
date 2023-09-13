09d3daadeea159e366195c2ba6b9ad78feef3f50
DannyVogel <danny.hj.vogel@gmail.com>
2023-09-13 22:44:40 +0200

    style: update home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,11 +7,13 @@ const authStore = useAuthStore()
 <template>
   <main class="h-full py-12 px-3 flex flex-col items-center gap-2 bg-gray-800 text-white">
     <h1 class="text-4xl font-title animate-pulse">SoundScribe</h1>
-    <p>Where music meets storytelling.</p>
-    <p>Check out the latest Notes in the Feed or search for a specific Scribe below.</p>
-    <p>
+    <p class="text-center">Where music meets storytelling.</p>
+    <p class="text-center">
+      Check out the latest Notes in the Feed or search for a specific Scribe below.
+    </p>
+    <p class="text-center">
       You can log in or create an account in the side menu by clicking the icon on the top-right.
     </p>
-    <SearchScribe />
+    <SearchScribe class="mt-4" />
   </main>
 </template>
