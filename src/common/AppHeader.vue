5eeb6ee09e93314610d2dd4eb133cf3acf045cb4
dvogelg4al <134928350+dvogelg4al@users.noreply.github.com>
2023-10-03 20:20:04 +0200

    style: header design update

--- src/common/AppHeader.vue
+++ src/common/AppHeader.vue
@@ -13,7 +13,7 @@ const isSideNavShown = ref(false)
   <header
     class="h-full px-2 flex justify-between items-center bg-gray-900 border-b-2 border-orange-600"
   >
-    <div class="w-full flex flex-col justify-start">
+    <div class="w-full flex items-center justify-between">
       <h1
         @click="router.push('/')"
         class="w-fit font-title text-white text-2xl cursor-pointer hover:animate-pulse"
@@ -22,21 +22,26 @@ const isSideNavShown = ref(false)
         SoundScribe
       </h1>
       <NavBar />
-    </div>
-    <div class="flex items-start">
-      <button @click="isSideNavShown = true" class="w-8 h-8">
-        <Bars3Icon class="text-white" />
-      </button>
-      <div
-        @click="isSideNavShown = false"
-        class="absolute w-screen h-screen top-0 left-0 opacity-40 bg-slate-800"
-        :class="!isSideNavShown && 'hidden'"
-      ></div>
-      <SideNav
-        @close="isSideNavShown = false"
-        class="w-64 h-full p-3 fixed top-0 right-0 bg-gray-900 shadow-md shadow-gray-700 translate-x-0 transition-transform ease-in-out duration-300 flex flex-col justify-start"
-        :class="!isSideNavShown && 'translate-x-full'"
-      />
+      <div class="flex items-start">
+        <button
+          @click="isSideNavShown = true"
+          class="w-8 h-8"
+          title="side menu button"
+          aria-label="side menu button"
+        >
+          <Bars3Icon class="text-white" />
+        </button>
+        <div
+          @click="isSideNavShown = false"
+          class="absolute w-screen h-screen top-0 left-0 opacity-40 bg-slate-800"
+          :class="!isSideNavShown && 'hidden'"
+        ></div>
+        <SideNav
+          @close="isSideNavShown = false"
+          class="w-64 h-full p-3 fixed top-0 right-0 bg-gray-900 shadow-md shadow-gray-700 translate-x-0 transition-transform ease-in-out duration-300 flex flex-col justify-start"
+          :class="!isSideNavShown && 'translate-x-full'"
+        />
+      </div>
     </div>
   </header>
 </template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,13 +4,13 @@ import useAuthStore from '@/stores/authStore'
 const authStore = useAuthStore()
 </script>
 <template>
-  <nav class="flex justify-center gap-4 text-sm text-white">
+  <nav class="flex justify-center gap-2 text-sm text-white">
     <RouterLink to="/feed">Feed</RouterLink>
     <span>{{ ' | ' }}</span>
     <RouterLink :to="{ name: 'soundBoard', params: { user: authStore.userName } }"
       >SoundBoard
     </RouterLink>
-    <span>{{ ' | ' }}</span>
-    <RouterLink to="/discover">Discover</RouterLink>
+    <span v-if="false">{{ ' | ' }}</span>
+    <RouterLink v-if="false" to="/discover">Discover</RouterLink>
   </nav>
 </template>
